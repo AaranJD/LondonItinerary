36387927dfb1e23c558fbd42d30703fcd080dee7
AaranJD <37222055+AaranJD@users.noreply.github.com>
2021-07-31 13:37:43 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,100 @@
+var activityLocations = [
+  // ['Portobello Market', 51.5223,-0.2069, 'market', 2],
+  ['Holland Park', 51.504,-0.205, 'park', 2],
+  ['Science Museum', 51.4980706,-0.1788774, 'museum', 2],
+  ['Kensington Gardens', 51.506,-0.18, 'park', 2],
+  ['Hyde Park', 51.505,-0.165, 'park', 2],
+  ['Natural History Museum', 51.496715,-0.1785559, 'museum', 2],
+  ['Harrods', 51.499,-0.1628535, 'architecture', 2],
+  ['V&A Museum', 51.4966392,-0.1743687, 'museum', 2],
+  ['Camden Market', 51.5408679,-0.1463053, 'market', 1],
+  ['Regents Park', 51.5312705,-0.1591581, 'park', 1],
+  ['Wallace Collection', 51.5172722,-0.155, 'art', 1],
+  ['British Museum', 51.5187318,-0.1259695, 'museum', 1],
+  ['Wellcome Collection', 51.5259044,-0.1335, 'art', 1],
+  ['The British Library', 51.5299996,-0.1285, 'library', 1],
+  ['Covent Garden', 51.5121138,-0.123, 'market', 3],
+  ['St Jamess Park', 51.501,-0.135, 'park', 3],
+  ['Westminster Abbey', 51.499382,-0.1275, 'church', 3],
+  ['Houses of Parliament', 51.4994795,-0.125, 'architecture', 3],
+  ['Tate Britain', 51.4910621,-0.1287, 'art', 3],
+  ['Westminster Cathedral', 51.4958094,-0.14, 'church', 3],
+  ['Buckingham Palace', 51.5007075,-0.1431824, 'architecture', 3],
+  ['Royal Academy of Arts', 51.5094426,-0.1395, 'art', 3],
+  ['National Gallery', 51.508929,-0.128, 'art', 3],
+  ['Borough Market', 51.5053,-0.092, 'market', 4],
+  ['Tate Modern', 51.5077,-0.099, 'art', 4],
+  ['St Pauls Cathedral', 51.5138453,-0.0985, 'church', 4],
+  ['Sky Gardens', 51.511322,-0.0832, 'architecture', 4],
+  ['St Dunsten in the East', 51.5096594,-0.0830888, 'church', 4],
+  ['Leadenhall Market', 51.512766,-0.0833, 'market', 4],
+  ['Barbican Conservatory', 51.520003,-0.0937093, 'park', 4]
+]
+
+// var foodLocations = [
+//   ['Wok Boy', 'lat', 'long', 'Camden Market', 'description', 'website', 'menu'],
+//   ['Oli Babas', 'lat', 'long', 'Camden Market', 'description', 'website', 'menu'],
+//   ['StakeHaus', 'lat', 'long', 'Camden Market', 'description', 'website', 'menu'],
+//   ['Traditional fish and chips', 'lat', 'long', 'Camden Market', 'description', 'website', 'menu'],
+//   ['Mac Factory', 'lat', 'long', 'Camden Market', 'description', 'website', 'menu'],
+//   ['The Cheese Wheel', 'lat', 'long', 'Camden Market', 'description', 'website', 'menu'],
+//   ['Indian Alley', 'lat', 'long', 'Camden Market', 'description', 'website', 'menu'],
+//   ['Yorkshire Burrito', 'lat', 'long', 'Camden Market', 'description', 'website', 'menu'],
+//   ['Camden Coffee', 'lat', 'long', 'Camden Market', 'description', 'website', 'menu'],
+//   ['Brindisa', 'lat', 'long', 'Borough Market', 'description', 'website', 'menu'],
+//   ['El Pastor', 'lat', 'long', 'Borough Market', 'description', 'website', 'menu'],
+//   ['Padella', 'lat', 'long', 'Borough Market', 'description', 'website', 'menu'],
+//   ['Kappcasein', 'lat', 'long', 'Borough Market', 'description', 'website', 'menu'],
+//   ['Bread Ahead', 'lat', 'long', 'Borough Market', 'description', 'website', 'menu'],
+//   ['The Ginger Pig', 'lat', 'long', 'Borough Market', 'description', 'website', 'menu'],
+//   ['Hobbs Meat Roast', 'lat', 'long', 'Borough Market', 'description', 'website', 'menu']
+// ];
+
+var photoLocations = [
+  ['Millennium Bridge', 51.5095286,-0.1007097, 'photo', 'Thames Embankment'],
+  ['K2 Telephone Boxes', 51.5125071,-0.125681, 'photo', 'Covent Garden'],
+  ['Big Ben', 51.5007286,-0.1267926, 'photo', 'Westminster'],
+  ['Notting Hill Colourful Houses', 51.5200409,-0.2106757, 'photo', 'Nottinghill'],
+  ['Neals Yard', 51.5144724,-0.1285418, 'photo', 'Covent Garden'],
+  ['Tower Bridge', 51.5054559,-0.0775237, 'photo', 'Camden Market'],
+  ['Cable Car', 51.5077134,0.0169177, 'photo', 'Camden Market'],
+  ['The Churchill Arms', 51.5069112,-0.1958739, 'photo', 'Kengsington'],
+  ['Platform 9 3/4', 51.5322391,-0.1251, 'photo', 'Camden Market'],
+  ['Abbey Road', 51.5370921,-0.1855463, 'photo', 'Borough Market'],
+  ['Leake Street Graffiti Tunnel', 51.5014539,-0.1159545, 'photo', 'Waterloo'],
+  ['Piccadilly Circus', 51.5099975,-0.1360747, 'photo', 'Borough Market'],
+  ['Changing of the Guard', 51.5011674,-0.1443206, 'photo', 'Buckingham Palace'],
+  ['London Eye', 51.5032968,-0.1206266, 'photo', 'Waterloo'],
+  ['Trafalgar Square', 51.5080899,-0.1291272, 'photo', 'Borough Market']
+]
+
+var mewsLocations = [
+  ['Bathurst Mews', 51.5136969,-0.1745801, 'mews', 'Thames Embankment'],
+  ['Drayson Mews', 51.5021422,-0.1940841, 'mews', 'Covent Garden'],
+  ['Ennismore Mews', 51.4996567,-0.1696104, 'mews', 'Westminster'],
+  ['Ennismore Garden', 51.4991199,-0.1720693, 'mews', 'Nottinghill'],
+  ['Sussex Mews West', 51.5127978,-0.1749623, 'mews', 'Covent Garden'],
+  ['Osten Mews', 51.4962366,-0.1896077, 'mews', 'Camden Market'],
+  ['Conduit Mews', 51.5146201,-0.1770291, 'mews', 'Camden Market'],
+  ['Ensor Mews', 51.4900724,-0.1788065, 'mews', 'Kengsington'],
+  ['Atherstone Mews', 51.4955752,-0.1822346, 'mews', 'Camden Market'],
+  ['Kynance Mews', 51.4973848,-0.1852674, 'mews', 'Borough Market'],
+  ['Holland Park Mews', 51.5058083,-0.2066657, 'mews', 'Holland Park'],
+  ['Spear Mews', 51.4927201,-0.1950326, 'mews', 'Borough Market'],
+  ['Hyde Park Garden Mews', 51.5131966,-0.1715597, 'mews', 'Buckingham Palace'],
+  ['Adam and Eve Mews', 51.4993742,-0.1949488, 'mews', 'Waterloo'],
+  ['Creswell Place', 51.4892977,-0.1830561, 'mews', 'Borough Market'],
+  ['Kendrick Mews', 51.4934827,-0.1773176, 'mews', 'Camden Market'],
+  ['St Lukes Mews', 51.5177784,-0.2039214, 'mews', 'Nottinghill'],
+  ['Reece Mews', 51.4934771,-0.1777129, 'mews', 'Camden Market'],
+  ['Stanhope Mews', 51.4944495,-0.1825356, 'mews', 'Borough Market'],
+  ['Redcliffe Mews', 51.4869503,-0.187344, 'mews', 'Waterloo'],
+  ['Queens Gate Mews', 51.4990919,-0.1826777, 'mews', 'Borough Market'],
+  ['Cornwall Mews', 51.4962564,-0.1853001, 'mews', 'Buckingham Palace'],
+  ['South Warren Mews', 51.5233518,-0.1422704, 'mews', 'Waterloo'],
+  ['Colville Mews', 51.5153907,-0.2008792, 'mews', 'Nottinghill'] 
+]
+
 var Day = "*";
 // Get the container element
 var btnContainer = document.getElementById("buttonRow");
@@ -25,6 +122,29 @@ for (var i = 0; i < tabs.length; i++) {
   });
 }
 
+//LIST OF ACTIVITIES
+var activitiesContainer = document.getElementById("activitiesContainer");
+var numberofdays = 4;
+
+for (var i = 1; i < numberofdays + 1; i++) {
+  var dayContainer = document.createElement('div');
+  var dayHeader = document.createElement('h6');
+  dayHeader.innerHTML = ("Day " + i);
+  dayContainer.setAttribute("class", ("Day " + i));
+  dayContainer.setAttribute("id", i);
+  dayContainer.appendChild(dayHeader);
+  activitiesContainer.appendChild(dayContainer);
+}
+
+for (var i = 0; i < activityLocations.length; i++) {
+  var spanObj = document.createElement('span');
+  spanObj.innerHTML = activityLocations[i][0];
+ 
+  var dayNumber = activityLocations[i][4];
+  var dayCon = document.getElementById(dayNumber);
+  dayCon.appendChild(spanObj);
+}
+
 //Filter the list by days
 function filterDay(e) {
   let filter = e.target.dataset.filter; // grab the value in the event target's data-filter attribute
@@ -43,12 +163,33 @@ function filterDay(e) {
       }
     }
   } 
-  initMap();
+  
+  hideMarkers();
+  var x, y;
+
+  if (Day == "*") {
+    x = 0;
+    y = activityLocations.length;
+  } else if (Day == "1") {
+    x = 8;
+    y = 14;
+  } else if (Day == "2") {
+    x = 0;
+    y = 8;
+  } else if (Day == "3") {
+    x = 14;
+    y = 23;
+  } else if (Day == "4") {
+    x = 23;
+    y = activityLocations.length;
+  }
+  addMarkers(activityLocations, x, y);
 };
 
 //Filter the list by tab options
 function tabFilter(e) {
   var items = document.getElementsByClassName('Tab'), i;
+  hideMarkers();
 
   for (i = 0; i < items.length; i += 1) {
     if (items[i].className != ("Tab " + e)) {
@@ -57,16 +198,11 @@ function tabFilter(e) {
       items[i].style.display = 'grid';
     }
   } 
+
+  if (e == "Activities")  { addMarkers(activityLocations, 0, activityLocations.length); }
+  else if (e == "Photos") { addMarkers(photoLocations, 0, photoLocations.length); }
+  else if (e == "Mews")   { addMarkers(mewsLocations, 0, mewsLocations.length); }
 };
-
-// //Filter the list by tab
-// function foodtabFilter() {
-//   var day = document.getElementsByClassName('Day'), i;
-
-//   for (i = 0; i < day.length; i += 1) {
-//     day[i].style.display = 'none';
-//   }
-// };
 
 //Filter the list by types
 function filterTypes(e) {
@@ -130,104 +266,15 @@ function CenterControl(controlDiv, map) {
   // Setup the click event listeners: simply set the map to London.
   controlUI.addEventListener("click", () => {
     map.setCenter(london);
-    map.setZoom(13);
+    map.setZoom(12.9);
   });
 }
 
-// function LegendControl(legendDiv, map) {
-//   const legendUI = document.createElement("div");
-//   legendUI.style.backgroundColor = "#fff";
-//   legendUI.style.border = "2px solid #fff";
-//   legendUI.style.borderRadius = "3px";
-//   legendUI.style.boxShadow = "0 2px 6px rgba(0,0,0,.3)";
-//   legendUI.style.cursor = "pointer";
-//   legendUI.style.marginTop = "8px";
-//   legendUI.style.marginBottom = "22px";
-//   legendUI.style.textAlign = "center";
-//   legendUI.title = "Click to recenter the map";
-//   legendDiv.appendChild(legendUI);
-
-//   var legendTitles = [
-//     ['Museum'],
-//     ['Art Gallery'],
-//     ['Market'],
-//     ['Park'],
-//     ['Architecture'],
-//     ['Church'],
-//     ['Library']
-//   ]
-
-//   for (i = 0; i < 7; i++) {
-//     const legendText = document.createElement("div");
-//     legendText.style.color = "rgb(25,25,25)";
-//     legendText.style.fontFamily = "Roboto,Arial,sans-serif";
-//     legendText.style.fontSize = "16px";
-//     legendText.style.lineHeight = "38px";
-//     legendText.style.paddingLeft = "5px";
-//     legendText.style.paddingRight = "5px";
-//     legendText.innerHTML = "http://maps.google.com/mapfiles/ms/icons/red-dot.png" + legendTitles[i];
-//     legendUI.appendChild(legendText);
-//   }
-  
-
-// //   .gm-style-iw-d {
-// //     color: black;
-// // }
-
-// // #legend {
-// //     background: #fff;
-// //     color: black;
-// //     padding: 8px;
-// //     margin-left: 10px;
-// //     border: 1px solid lightgray;
-// //     border-radius: 3px;
-// //     box-shadow: 0 2px 6px rgba(0,0,0,.3);
-// // }
-
-// // #legend img {
-// //     width: 24px;
-// //     margin: 1px;
-// // }
-// }
-
 // Initialize and add the map
-function initMap() {
-    //Map Locations
-    var locations = [
-      ['Portobello Market', 51.5223,-0.2069, 'market', 1],
-      ['Holland Park', 51.504,-0.205, 'park', 2],
-      ['Science Museum', 51.4980706,-0.1788774, 'museum', 3],
-      ['Kensington Gardens', 51.506,-0.18, 'park', 4],
-      ['Hyde Park', 51.505,-0.165, 'park', 5],
-      ['Natural History Museum', 51.496715,-0.1785559, 'museum', 6],
-      ['Harrods', 51.499,-0.1628535, 'architecture', 7],
-      ['V&A Museum', 51.4966392,-0.1743687, 'museum', 8],
-      ['Camden Market', 51.5408679,-0.1463053, 'market', 9],
-      ['Regents Park', 51.5312705,-0.1591581, 'park', 10],
-      ['Wallace Collection', 51.5172722,-0.155, 'art', 11],
-      ['British Museum', 51.5187318,-0.1259695, 'museum', 12],
-      ['Wellcome Collection', 51.5259044,-0.1335, 'art', 13],
-      ['The British Library', 51.5299996,-0.1285, 'library', 14],
-      ['Covent Garden', 51.5121138,-0.123, 'market', 15],
-      ['St Jamess Park', 51.501,-0.135, 'park', 16],
-      ['Westminster Abbey', 51.499382,-0.1275, 'church', 17],
-      ['Houses of Parliament', 51.4994795,-0.125, 'architecture', 18],
-      ['Tate Britain', 51.4910621,-0.1287, 'art', 19],
-      ['Westminster Cathedral', 51.4958094,-0.14, 'church', 20],
-      ['Buckingham Palace', 51.5007075,-0.1431824, 'architecture', 21],
-      ['Royal Academy of Arts', 51.5094426,-0.1395, 'art', 22],
-      ['National Gallery', 51.508929,-0.128, 'art', 23],
-      ['Borough Market', 51.5053,-0.092, 'market', 24],
-      ['Tate Modern', 51.5077,-0.099, 'art', 25],
-      ['St Pauls Cathedral', 51.5138453,-0.0985, 'church', 26],
-      ['Sky Gardens', 51.511322,-0.0832, 'architecture', 27],
-      ['St Dunsten in the East', 51.5096594,-0.0830888, 'church', 28],
-      ['Leadenhall Market', 51.512766,-0.0833, 'market', 29],
-      ['Barbican Conservatory', 51.520003,-0.0937093, 'park', 30]
-    ]
+function initMap() {  
     // The map, centered at london
     map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 13,
+      zoom: 12.9,
       center: london,
     });
     //Airbnb Marker
@@ -244,6 +291,20 @@ function initMap() {
       title: "Airbnb",
       icon: icon
     });
+    //Kings Cross Station Marker - from https://fontawesome.com/v5.15/icons/subway?style=solid
+    var stationIcon = {
+      url: "Images/subway-solid.svg", // url
+      scaledSize: new google.maps.Size(24, 24), // scaled size
+      origin: new google.maps.Point(0,0), // origin
+      anchor: new google.maps.Point(0, 0) // anchor
+    };
+
+    new google.maps.Marker({
+      position: new google.maps.LatLng(51.5320166,-0.1235673),
+      map,
+      title: "King's Cross Station",
+      icon: stationIcon
+    });
     // Create the DIV to hold the control and call the CenterControl()
     // constructor passing in this DIV.
     const centerControlDiv = document.createElement("div");
@@ -252,34 +313,6 @@ function initMap() {
 
     // Maps legend
     const icons = {
-      // museum: {
-      //   name: "Museum",
-      //   icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
-      // },
-      // gallery: {
-      //   name: "Art Gallery",
-      //   icon: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png",
-      // },
-      // park: {
-      //   name: "Park",
-      //   icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
-      // },
-      // market: {
-      //   name: "Market",
-      //   icon: "http://maps.google.com/mapfiles/ms/icons/orange-dot.png",
-      // },
-      // architecture: {
-      //   name: "Architecture",
-      //   icon: "http://maps.google.com/mapfiles/ms/icons/pink-dot.png",
-      // },
-      // church: {
-      //   name: "Church",
-      //   icon: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png",
-      // },
-      // library: {
-      //   name: "Library",
-      //   icon: "http://maps.google.com/mapfiles/ms/icons/ltblue-dot.png",
-      // },
       museum: {
         name: "Museum",
         icon: "Images/museumIcon3.svg",
@@ -310,63 +343,6 @@ function initMap() {
       },
     };
 
-    // Map Markers
-    var infowindow = new google.maps.InfoWindow();
-    var marker, i, x, y;
-    if (Day == "*") {
-      x = 0;
-      y = locations.length;
-    } else if (Day == "One") {
-      x = 8;
-      y = 14;
-    } else if (Day == "Two") {
-      x = 0;
-      y = 8;
-    } else if (Day == "Three") {
-      x = 14;
-      y = 23;
-    } else if (Day == "Four") {
-      x = 23;
-      y = locations.length;
-    }
-    for (i = x; i < y; i++) { 
-      let url;
-      if      (locations[i][3] == 'museum')       { url = "Images/museumIcon3.svg" } 
-      else if (locations[i][3] == 'art')          { url = "Images/artgallery.svg" } 
-      else if (locations[i][3] == 'park')         { url = "Images/parkIcon.svg" } 
-      else if (locations[i][3] == 'market')       { url = "Images/marketIcon.svg" } 
-      else if (locations[i][3] == 'library')      { url = "Images/libraryIcon.svg" } 
-      else if (locations[i][3] == 'church')       { url = "Images/churchIcon.svg" } 
-      else if (locations[i][3] == 'architecture') { url = "Images/architectureIcon.svg" } 
-      // if      (locations[i][3] == 'museum')       { url = "http://maps.google.com/mapfiles/ms/icons/red-dot.png" } 
-      // else if (locations[i][3] == 'art')          { url = "http://maps.google.com/mapfiles/ms/icons/purple-dot.png" } 
-      // else if (locations[i][3] == 'park')         { url = "http://maps.google.com/mapfiles/ms/icons/green-dot.png" } 
-      // else if (locations[i][3] == 'market')       { url = "http://maps.google.com/mapfiles/ms/icons/orange-dot.png" } 
-      // else if (locations[i][3] == 'library')      { url = "http://maps.google.com/mapfiles/ms/icons/ltblue-dot.png" } 
-      // else if (locations[i][3] == 'church')       { url = "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png" } 
-      // else if (locations[i][3] == 'architecture') { url = "http://maps.google.com/mapfiles/ms/icons/pink-dot.png" } 
-
-      
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map,
-        icon: {
-          url: url,
-          scaledSize: new google.maps.Size(30, 30), // scaled size
-        }
-      });
-
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent(
-            "<p>" + locations[i][0] + "<br />" + 
-                    locations[i][3] + "</p>"
-          );
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-    }
-
     const legend = document.getElementById("legend");
 
     for (const key in icons) {
@@ -377,18 +353,60 @@ function initMap() {
       div.innerHTML = '<img src="' + icon + '"> ' + name;
       legend.appendChild(div);
     }
-
     map.controls[google.maps.ControlPosition.LEFT].push
       (document.getElementById('legend'));
+
+    addMarkers(activityLocations, 0, activityLocations.length);
 }
 
-// Adds a marker to the map and push to the array.
-function addMarker(position) {
-  const marker = new google.maps.Marker({
-    position,
-    map,
-  });
-  markers.push(marker);
+function addMarkers(locations, x, y) {
+  var i;
+  let url;
+  
+  for ( i = x; i < y; i++) {
+    if      (locations[i][3] == 'museum')       { url = "Images/museumIcon3.svg" } 
+    else if (locations[i][3] == 'art')          { url = "Images/artgallery.svg" } 
+    else if (locations[i][3] == 'park')         { url = "Images/parkIcon.svg" } 
+    else if (locations[i][3] == 'market')       { url = "Images/marketIcon.svg" } 
+    else if (locations[i][3] == 'library')      { url = "Images/libraryIcon.svg" } 
+    else if (locations[i][3] == 'church')       { url = "Images/churchIcon.svg" } 
+    else if (locations[i][3] == 'architecture') { url = "Images/architectureIcon.svg" } 
+    else if (locations[i][3] == 'photo')        { url = "Images/photoIcon.svg"}
+    else if (locations[i][3] == 'mews')         { url = "Images/mewsIcon.svg"}
+
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      map: map,
+      icon: {
+        url: url,
+        scaledSize: new google.maps.Size(30, 30), // scaled size
+      }
+    }); 
+
+    marker['infowindow'] = new google.maps.InfoWindow({
+      content: "<p>" + locations[i][0] + "<br />" + 
+                       locations[i][3] + "</p>"
+    });
+
+    google.maps.event.addListener(marker, 'mouseover', function() {
+      this['infowindow'].open(map, this);
+    });
+
+    google.maps.event.addListener(marker, 'mouseout', function() {
+      this['infowindow'].close(map, this);
+    });
+    // google.maps.event.addListener(marker, 'click', (function(marker, i) {
+    //   return function() {
+    //     infowindow.setContent(
+    //       "<p>" + locations[i][0] + "<br />" + 
+    //               locations[i][3] + "</p>"
+    //     );
+    //     infowindow.open(map, marker);
+    //   }
+    // })(marker, i));
+
+    markers.push(marker);
+  }
 }
 
 // Sets the map on all markers in the array.
